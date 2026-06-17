a978de66521f9364eadd349594bf61429bd1b78a
Vishesh Jain <visheshjain@Visheshs-MacBook-Air.local>
2026-06-17 13:00:09 +0530

    added a microservice for email dployed on vercel.

--- verification/service.py
+++ verification/service.py
@@ -2,12 +2,13 @@ from django.core.mail import send_mail
 from django.conf import settings
 import random
 import os
+import requests
 import mailtrap as mt
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from rest_framework.exceptions import ValidationError
-
+import json
 
 
 def EmailVerification(request,email,user):
@@ -21,6 +22,21 @@ def EmailVerification(request,email,user):
         body=f'your link is {link}'
         email_from = settings.EMAIL_HOST_USER
         recipient_list = [email]   
+        try:
+            url='https://email-microservice-flame.vercel.app/send-mail/'
+            data={
+                'subject':subject,
+                'body':body,
+                'email_from':email_from,
+                'recipient_list':recipient_list
+            }
+            data_json=json.dumps(data)
+            status=requests.post(url,data=data_json)
+            print(status)
+        except Exception as e:
+            raise ValidationError(f'the microservice for email is not workning.{str(e)}')
+
+        
         send_mail( subject, body, email_from, recipient_list )  
     except Exception as e:
         raise ValidationError(f'the email could not be sent.{str(e)}')
